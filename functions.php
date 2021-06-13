5848afdcf0e7ce4de47d0dcd18af1c17e7f1b55a
Muhammad Zohaib <zohaib87@live.com>
2021-06-13 22:14:55 +0500

    Minor bug fixes

--- functions.php
+++ functions.php
@@ -1,8 +1,8 @@
 <?php
 
-function _xe_child_enqueue_scripts() {
+function samaaj_child_enqueue_scripts() {
 
-  wp_enqueue_style( '_xe-child-style', get_stylesheet_uri() );
+  wp_enqueue_style( 'samaaj-child-style', get_stylesheet_uri() );
 
 }
-add_action('wp_enqueue_scripts', '_xe_child_enqueue_scripts');
+add_action('wp_enqueue_scripts', 'samaaj_child_enqueue_scripts');
